0b23c3a171d1573b2a5d9d81147072a84c567a67
Kadha Manikanta <kadhamanikanta284@gmail.com>
2025-09-20 22:18:14 +0000

    same error

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
@@ -12,12 +12,7 @@ const firebaseConfig = {
   appId: "1:611451925537:web:bbf217000f9c49aedf9f60",
 };
 
-// Initialize Firebase
-let app;
-if (!getApps().length) {
-  app = initializeApp(firebaseConfig);
-} else {
-  app = getApps()[0];
-}
+// Initialize Firebase for SSR
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 export const auth = getAuth(app);
