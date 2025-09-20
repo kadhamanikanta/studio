6f169ea3c241c290ad0f7909d0500059ca32d7da
Kadha Manikanta <kadhamanikanta284@gmail.com>
2025-09-20 22:17:14 +0000

    it showing firebase auth/configuration not found

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "studio-2852089076-17daf",
-  "appId": "1:611451925537:web:bbf217000f9c49aedf9f60",
-  "apiKey": "AIzaSyBqeou5fyh8AogmTYxEWpfGYp5wLdc2LZc",
-  "authDomain": "studio-2852089076-17daf.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "611451925537"
+  apiKey: "AIzaSyBqeou5fyh8AogmTYxEWpfGYp5wLdc2LZc",
+  authDomain: "studio-2852089076-17daf.firebaseapp.com",
+  projectId: "studio-2852089076-17daf",
+  storageBucket: "studio-2852089076-17daf.appspot.com",
+  messagingSenderId: "611451925537",
+  appId: "1:611451925537:web:bbf217000f9c49aedf9f60",
 };
 
 // Initialize Firebase
