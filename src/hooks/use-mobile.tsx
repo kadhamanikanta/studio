3406ea5b0d8665da44300640e815287bfec42b7c
Kadha Manikanta <kadhamanikanta284@gmail.com>
2025-09-20 22:08:55 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -1,21 +1,24 @@
+'use client';
+
 import * as React from 'react';
 
 const MOBILE_BREAKPOINT = 768;
 
 export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState(false);
+  const [hasMounted, setHasMounted] = React.useState(false);
 
   React.useEffect(() => {
+    setHasMounted(true);
     const checkDevice = () => {
       setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
     };
 
-    // Check on initial mount on the client side only
     checkDevice();
 
     window.addEventListener('resize', checkDevice);
     return () => window.removeEventListener('resize', checkDevice);
   }, []);
 
-  return isMobile;
+  return hasMounted ? isMobile : false;
 }
